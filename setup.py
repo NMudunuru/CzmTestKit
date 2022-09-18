241e52e5060f733c47b9568ab9ce00bd8009a9f3
NMudunuru <s.n.r.mudunuru@gmail.com>
2022-09-18 17:50:15 +0200

    v1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='czmtestkit',
-    version='1.0.0',
+    version='1.1.0',
     description='Python + abaqus-python based package for testing Abaqus/CAE user element subrotines for cohesive zone models',
     long_description=long_description,
     long_description_content_type='text/markdown',
